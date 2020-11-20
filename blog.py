574a699d7d6d4e08004f0f3288abcdc5afb762fc
Tommaso Perondi <tommaso@tommasos-mbp-001.homenet.telecomitalia.it>
2020-11-20 17:44:06 +0100

    Update: 	added templates for all pages with extends to base.html

--- blog.py
+++ blog.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, request
-blog = Flask(__name__)
+blog = Flask(__name__,
+            static_url_path='',
+            static_folder='static',
+            template_folder='templates/')
 
 @blog.route('/')
 def home():
     return render_template('index.html')
+
+@blog.route('/blog')
+def blogpage():
+    return render_template('blog.html')
+
+@blog.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    blog.run(debug = True)
